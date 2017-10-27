1f0d15cf79dcd02196bf471b7c836e2fa191bd39
Mantas Kemėšius <mantas@Mantass-Air.lan>
2017-10-27 19:13:43 +0300

    Check faker at defaultController

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Faker\Factory;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,4 +17,18 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig');
     }
+
+    /**
+     * @Route("/faker", name="faker")
+     */
+    public function fakerAction(Request $request)
+    {
+        $faker = Factory::create();
+
+        for ($i = 0 ; $i <10 ; $i++ )
+            echo $faker->name.'<br>';
+
+        die;
+    }
+
 }
