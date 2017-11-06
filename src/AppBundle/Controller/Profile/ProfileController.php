3835b9ec3a6ae57d6395b8b678bccdb9c264d57c
Mantas Kemėšius <mantas@Mantass-MacBook-Air.local>
2017-11-06 20:18:58 +0200

    Front-end for address crud

--- src/AppBundle/Controller/Profile/ProfileController.php
+++ src/AppBundle/Controller/Profile/ProfileController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller\Profile;
 
+use AppBundle\Entity\Address;
 use Faker\Factory;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,14 +14,77 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class ProfileController extends Controller
 {
-  /**
-   * @Route("/", name="profile_index")
-   */
-  public function indexAction(Request $request)
-  {
-      // replace this example code with whatever you need
-      return $this->render('Profile/profile.html.twig');
-  }
+
+    /**
+     * Creates a new address entity.
+     *
+     * @Route("/", name="profile_index")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $address_repository = $em->getRepository('AppBundle:Address');
+        $addresses = $address_repository->findBy(array('shop_owner' => $this->getUser()->getId()));
+
+        $address = new Address();
+        $address->setShopOwner($this->getUser());
+        $form = $this->createForm('AppBundle\Form\AddressType', $address);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($address);
+            $em->flush();
+
+            return $this->redirectToRoute('profile_index', array('id' => $address->getId()));
+        }
+
+        return $this->render('Profile/profile.html.twig', array(
+            'shops' => $addresses,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing address entity.
+     *
+     * @Route("/{id}/edit", name="profile_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Address $address)
+    {
+        $editForm = $this->createForm('AppBundle\Form\AddressType', $address);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('profile_index');
+        }
+
+        return $this->render('profile/edit.html.twig', array(
+            'shops' => $address,
+            'form' => $editForm->createView(),
+        ));
+    }
+
+
+    /**
+     * Deletes a address entity.
+     *
+     * @Route("/{id}", name="profile_delete")
+     * @Method({"GET", "POST"})
+     */
+    public function deleteAction(Request $request, Address $address)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($address);
+        $em->flush();
+        return $this->redirectToRoute('profile_index');
+    }
+
 
 
 }
