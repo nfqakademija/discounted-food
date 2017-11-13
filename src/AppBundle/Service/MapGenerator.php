63dfb9ad54e46166ae8767a0b05091dcd6ee51dd
t0000678 <t0000678@t0000678.nfq.lt>
2017-11-13 20:30:18 +0200

    add infowindows

--- src/AppBundle/Service/MapGenerator.php
+++ src/AppBundle/Service/MapGenerator.php
@@ -2,6 +2,7 @@
 namespace AppBundle\Service;
 use Ivory\GoogleMap\Base\Coordinate;
 use Ivory\GoogleMap\Map;
+use Ivory\GoogleMap\Overlay\InfoWindow;
 use Ivory\GoogleMap\Overlay\Marker;
 
 class MapGenerator
@@ -15,12 +16,15 @@ class MapGenerator
         $map->setMapOption('zoom', 12);
         $map->setCenter(new Coordinate(54.687157, 25.279652));
 
+
         foreach($addresses as $address) {
             $marker = new Marker(new Coordinate($address->getLatitude(), $address->getLongitude()));
             $map->getOverlayManager()->addMarker($marker);
+            $infowindow = new InfoWindow($address->getAddress());
+            $marker->setInfoWindow($infowindow);
         }
         return $map;
     }
 
 
-}
+}   
